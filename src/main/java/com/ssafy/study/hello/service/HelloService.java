30e8eef9034c0589e46bf4eb8d4fa469bdaeb3bf
곽예경 <kyun9.k@gmail.com>
2026-05-06 20:51:09 +0900

    init

---
+++ src/main/java/com/ssafy/study/hello/service/HelloService.java
@@ -0,0 +1,10 @@
+package com.ssafy.study.hello.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+    public String hi(){
+        return "hi";
+    };
+}
